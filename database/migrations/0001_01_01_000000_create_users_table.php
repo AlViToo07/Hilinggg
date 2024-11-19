b22de34613d8c3de9c1f5a38b44cf2577fc57b7c
AlViToo07 <alvitoaldis@gmail.com>
2024-11-20 00:32:16 +0700

    menambah index migration dan routing pada project laravel

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,13 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
+        
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
@@ -35,6 +34,104 @@ return new class extends Migration
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
+
+        Schema::create('admin', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->unique()->constrained('users')->onDelete('cascade');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('address');
+            $table->string('telephone');
+            $table->string('img_profile')->nullable();
+            $table->timestamps();
+        });
+
+        
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->unique()->constrained('users')->onDelete('cascade');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('address');
+            $table->string('telephone');
+            $table->string('img_profile')->nullable();
+            $table->timestamps();
+        });
+
+        
+        Schema::create('province', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+
+        Schema::create('regency', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('province_id')->constrained('province')->onDelete('cascade');
+            $table->string('name');
+        });
+
+        
+        Schema::create('district', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('regency_id')->constrained('regency')->onDelete('cascade');
+            $table->string('name');
+        });
+
+        
+        Schema::create('destination', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('address');
+            $table->foreignId('regency_id')->constrained('regency')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        
+        Schema::create('hotel', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('hotel_url');
+            $table->string('address');
+            $table->foreignId('regency_id')->constrained('regency')->onDelete('cascade');
+            $table->timestamps();
+        });
+        
+
+
+        Schema::create('event', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('event_url');
+            $table->date('date');
+            $table->string('address');
+            $table->foreignId('regency_id')->constrained('regency')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        
+
+        Schema::create('review', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('customer')->onDelete('cascade');
+            $table->enum('item_type', ['Hotel', 'Wisata', 'Event']);
+            $table->unsignedBigInteger('item_id');
+            $table->integer('rating');
+            $table->text('review');
+            $table->timestamps();
+        });
+        
+
+        Schema::create('bookmark', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('customer')->onDelete('cascade');
+            $table->enum('item_type', ['Hotel', 'Wisata', 'Event']);
+            $table->unsignedBigInteger('item_id');
+            $table->timestamps();
+        });
+        
     }
 
     /**
